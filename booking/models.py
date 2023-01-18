7fce8f9333d0be9ad8f35d1efc0a501d2b77895f
OisinWrites <oisin.banville@gmail.com>
2023-01-18 19:02:01 +0000

    Create Reservation model

--- booking/models.py
+++ booking/models.py
@@ -8,7 +8,17 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=15)
 
+
 class Table(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     capacity = models.IntegerField()
     available = models.BooleanField(default=True)
+
+
+class Reservation(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(40)
