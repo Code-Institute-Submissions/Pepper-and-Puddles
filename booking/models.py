c7cceb5848fc23bdfdc1b1e04504e359212edca6
OisinWrites <oisin.banville@gmail.com>
2023-01-18 19:00:41 +0000

    Create Table model

--- booking/models.py
+++ booking/models.py
@@ -7,3 +7,8 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=25)
     address = models.CharField(max_length=200)
     phone = models.CharField(max_length=15)
+
+class Table(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    capacity = models.IntegerField()
+    available = models.BooleanField(default=True)
