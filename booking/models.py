99b64433eb03ddcd515eb3f2f1c98dca262f3886
OisinWrites <oisin.banville@gmail.com>
2023-01-18 18:59:38 +0000

    Create Restaurant class in booking app, models, changes staged properly

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=25)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
