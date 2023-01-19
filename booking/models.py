c7b48463191f3877e1bb9968fc7e23c14a5ece00
OisinWrites <oisin.banville@gmail.com>
2023-01-19 18:57:30 +0000

    Create guest_no_validation.py file to try to limit guests variable in bookings model

--- booking/models.py
+++ booking/models.py
@@ -1,11 +1,17 @@
 from django.db import models
-
+from guest_no_validation import Valid_Guest_Amount
 # Create your models here.
 
 
-class Booking(models.Model):
+class Booking(models.Model, guests):
     date = models.DateField()
     time = models.TimeField()
+    guests = models.IntegerField("Please tell us how many"
+                                 " guests are in your party.")
     name = models.CharField(max_length=30, blank=False)
     phone = models.CharField(max_length=15, blank=False)
     email = models.EmailField()
+    message = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
