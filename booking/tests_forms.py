a308012c2967ca950fb4178506c7820b9ae48131
OisinWrites <oisin.banville@gmail.com>
2023-01-23 16:45:16 +0000

    Create sample tests for form validation

--- booking/tests_forms.py
+++ booking/tests_forms.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .forms import BookingForm
 
-# Create your tests here.
+
+class TestBookingForm(TestCase):
+
+    def test_booking_name_is_required(self):
+        form = BookingForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required. ')
+
+    def test_guest_field_is_not_required(self):
+        form = BookingForm({'name': 'Test Booking'})
+        self.assertTrue(form.is_valid())
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = BookingForm()
+        self.assertEqual(form.Meta.fields, ['name', 'guests',
+                                            'date', 'phone', 'email'])
