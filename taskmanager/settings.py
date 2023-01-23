a3dbb0ea4e004663199243a6b5153271b8b7b05f
OisinWrites <oisin.banville@gmail.com>
2023-01-23 17:12:35 +0000

    Add Authentication_backends to settings

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -45,6 +45,11 @@ INSTALLED_APPS = [
     'booking'
 ]
 
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
 SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
