5e60cc5e6c037ca51d01a43da81c5de028887040
OisinWrites <oisin.banville@gmail.com>
2023-01-25 15:17:04 +0000

    Having env.py in the .gitignore file has stopped gitpod from saving the file, temporarily removing from .gitignore

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://xogjsxic:qZQ0ACLgbZhFIGkOmBtixYfJEGFTxCLr@kandula.db.elephantsql.com/xogjsxic"
+os.environ["SECRET_KEY"] = "I_u5ually_lose#myKeys"
+os.environ["CLOUDINARY_URL"] = "cloudinary://895331472263392:3dnb4C5D1QC1RY9A10Gx8KyGkWI@dwhennrjl"
