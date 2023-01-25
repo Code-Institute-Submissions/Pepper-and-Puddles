bc2fadc8d95c2cd92387718287013fe03bbe4b6d
OisinWrites <oisin.banville@gmail.com>
2023-01-25 14:45:03 +0000

    Create confirmed booking class with time selection logic, and validation raises

--- booking/models.py
+++ booking/models.py
@@ -23,7 +23,7 @@ class Table(model.Model):
     capacity = models.IntegerField()
     available = models.BooleanField(default=True)
 
-# Function to mark open table as booked, 
+# Function to mark open table as booked,
 # or return that it is already booked otherwise.
 
     def reserve(self):
@@ -33,3 +33,26 @@ class Table(model.Model):
             return True
         else:
             return False
+
+
+class Confirmed_Booking(model.Models):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        table = self.table
+        if table.available:
+            table.book()
+            super().save(*args, **kwargs)
+        else:
+            bookings = Confirmed_Booking.objects.filter(table=table)
+            for booking in bookings:
+                if (self.start_time < booking.end_time and
+                    self.start_time > booking.start_time) or (
+                 self.end_time < booking.end_time and
+                 self.end_time > booking.start_time):
+                    raise ValueError(
+                        "Table is already reserved during this time.")
+            table.reserve()
+            super().save(*args, **kwargs)
